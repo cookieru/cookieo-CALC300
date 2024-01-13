ea71f79680dba13fff15ac7b6871f9d3b99fb370
cookieru <ilyasukh00@mail.ru>
2024-01-13 12:47:31 +0500

    update script #08 less and retry functions

--- bjs/08_if_else/script.js
+++ bjs/08_if_else/script.js
@@ -22,33 +22,39 @@ const successAnswerPhrases = [
     "Victory!\n\u{1F638}"
 ];
 
-//Заменить методы window на что-нибудь из bootstrap (20 баллов)
-let minValue = parseInt(prompt('Минимальное знание числа для игры','0'));
-let maxValue = parseInt(prompt('Максимальное знание числа для игры','100'));
-alert(`Загадайте любое целое число от ${minValue} до ${maxValue}, а я его угадаю`);
-
-//Валидация введенных значений (через дизъюнкции) (10 баллов)
-
-//Ограничение по минимому и максимому для введенных значений (через тернарный оператор) (10 баллов)
-// мин = -999   макс = 999
-
-let answerNumber  = Math.floor((minValue + maxValue) / 2);
-let orderNumber = 1;
-let gameRun = true;
-
+//UI links
 const orderNumberField = document.querySelector('#orderNumberField');
 const answerField = document.querySelector('#answerField');
 
-orderNumberField.innerText = orderNumber;
-answerField.innerText = `${nextAnswerPhrases[0]} ${answerNumber }?`;
+//vars
+let minValue, maxValue, answerNumber, orderNumber;
+let gameRun;
+
+function Start()
+{
+    //Заменить методы window на что-нибудь из bootstrap (20 баллов)
+    minValue = parseInt(prompt('Минимальное знание числа для игры','0'));
+    maxValue = parseInt(prompt('Максимальное знание числа для игры','100'));
+    alert(`Загадайте любое целое число от ${minValue} до ${maxValue}, а я его угадаю`);
+
+    //Валидация введенных значений (через дизъюнкции) (10 баллов)
+    
+    //Ограничение по минимому и максимому для введенных значений (через тернарный оператор) (10 баллов)
+    // мин = -999   макс = 999
+
+    answerNumber  = Math.floor((minValue + maxValue) / 2);
+    orderNumber = 1;
+    gameRun = true;
+
+    orderNumberField.innerText = orderNumber++;
+    answerField.innerText = `${nextAnswerPhrases[0]} ${answerNumber }?`;
+}
+
+Start();
 
 // Рестарт программы
 document.querySelector('#btnRetry').addEventListener('click', function () {
-    //Переписать все
-    minValue = 0;
-    maxValue = 100;
-    orderNumber = 0;
-    gameRun = true;
+    Start();
 })
 
 // Кнопка больше
@@ -68,14 +74,12 @@ document.querySelector('#btnOver').addEventListener('click', function () {
             answerField.innerText = answerPhrase;
             gameRun = false;
         } else { //Продолжить поиск выше
-            minValue = answerNumber  + 1;
-            answerNumber  = Math.floor((minValue + maxValue) / 2);
-            orderNumber++;
+            minValue = answerNumber + 1;
+            answerNumber = Math.floor((minValue + maxValue) / 2);
+            orderNumberField.innerText = orderNumber++;            
 
             //Вывод числа в прописью, если строка получается менее 20 символов (15 баллов)
             
-            orderNumberField.innerText = orderNumber;
-
             const phraseRandom = Math.round( Math.random() * (nextAnswerPhrases.length - 1));
             answerField.innerText = `${nextAnswerPhrases[phraseRandom]} ${answerNumber }?`;
         }
@@ -83,7 +87,25 @@ document.querySelector('#btnOver').addEventListener('click', function () {
 })
 
 // Кнопка меньше
-// Написать функционал
+document.querySelector('#btnLess').addEventListener('click', function () {
+    if (gameRun){
+        if (minValue === maxValue){ // Игрок сжулничал и давал не правильные ответы
+            const phraseRandom = Math.round( Math.random() * (failAnswerPhrases.length - 1));
+            const answerPhrase = failAnswerPhrases[phraseRandom];            
+            answerField.innerText = answerPhrase;
+            gameRun = false;
+        } else { //Продолжить поиск ниже
+            maxValue = answerNumber - 1;
+            answerNumber = Math.floor((minValue + maxValue) / 2);
+            orderNumberField.innerText = orderNumber++;            
+
+            //Вывод числа в прописью, если строка получается менее 20 символов (15 баллов)
+            
+            const phraseRandom = Math.round( Math.random() * (nextAnswerPhrases.length - 1));
+            answerField.innerText = `${nextAnswerPhrases[phraseRandom]} ${answerNumber }?`;
+        }
+    }
+})
 
 // Кнопка Верно!
 document.querySelector('#btnEqual').addEventListener('click', function () {
