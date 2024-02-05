145181466cb131e966bd488e5011ae50e19e3803
cookieru <ilyasukh00@mail.ru>
2024-02-05 19:54:49 +0500

    update #10 firstname surname birthday gender generation

--- bjs/10_function_object/init.js
+++ bjs/10_function_object/init.js
@@ -2,6 +2,9 @@
 window.onload = function()
 {
     const initPerson = personGenerator.getPerson();
-    document.getElementById('firstNameOutput').innerText = initPerson.firstName;
+    document.querySelector('#firstNameOutput').innerText = initPerson.firstName;
+    document.querySelector('#surnameOutput').innerText = initPerson.surName;
+    document.querySelector('#genderOutput').innerText = initPerson.gender;
+    document.querySelector('#birthYearOutput').innerText = initPerson.birthday;
 };
 
--- bjs/10_function_object/personGenerator.js
+++ bjs/10_function_object/personGenerator.js
@@ -35,6 +35,21 @@ const personGenerator = {
             "id_10": "Андрей"
         }
     }`,
+    firstNameFemaleJson: `{
+        "count": 10,
+        "list": {     
+            "id_1": "Анастасия",
+            "id_2": "София",
+            "id_3": "Дарья",
+            "id_4": "Мария",
+            "id_5": "Александра",
+            "id_6": "Алиса",
+            "id_7": "Елизавета",
+            "id_8": "Полина",
+            "id_9": "Варвара",
+            "id_10": "Екатерина"
+        }
+    }`,
 
     GENDER_MALE: 'Мужчина',
     GENDER_FEMALE: 'Женщина',
@@ -49,22 +64,72 @@ const personGenerator = {
 
     randomFirstName: function() {
 
-        return this.randomValue(this.firstNameMaleJson);
+        if (this.person.gender === this.GENDER_MALE)
+        {
+            return this.randomValue(this.firstNameMaleJson);
+        }
+        else
+        {
+            return this.randomValue(this.firstNameFemaleJson);
+        }
 
     },
 
 
-     randomSurname: function() {
+    randomSurname: function() {
 
-        return this.randomValue(this.surnameJson);
+        let result = this.randomValue(this.surnameJson);
 
+        if (this.person.gender === this.GENDER_FEMALE)
+        {
+            result = result + "а";
+        }
+
+        return result;
+
+    },
+
+    randomGender: function() {
+
+        return this.randomIntNumber() === 1 ? this.GENDER_MALE : this.GENDER_FEMALE;
+
+    },
+
+    randomDate: function() {
+
+        const year = this.randomIntNumber(110, 70) + 1900;
+        const month = this.randomIntNumber(12, 1);
+
+        const isEven = month % 2 === 0;
+
+        let maxDay;
+
+        if (month === 2) 
+        {
+            const isLeap = year % 4 === 0;
+            maxDay = isLeap ? 29 : 28;
+        }
+        else if (month < 8) 
+        {
+            maxDay = isEven ? 30 : 31;
+        }
+        else 
+        {
+            maxDay = isEven ? 31 : 30;
+        }
+
+        const day = this.randomIntNumber(maxDay, 1);
+
+        return `${day}.${month}.${year}`;
     },
 
 
     getPerson: function () {
         this.person = {};
-        // this.person.gender = this.randomGender();
+        this.person.gender = this.randomGender();
         this.person.firstName = this.randomFirstName();
+        this.person.surName = this.randomSurname();
+        this.person.birthday = this.randomDate();
         return this.person;
     }
 };
